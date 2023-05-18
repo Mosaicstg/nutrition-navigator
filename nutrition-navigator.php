bb18b7ede9efa8d4a19704433a34a029d5c9d49e
Cesar Hernandez <cesar.hernandez1094@gmail.com>
2023-05-18 12:44:59 -0400

    refactor: rename file

--- nutrition-navigator.php
+++ nutrition-navigator.php
@@ -7,6 +7,6 @@
  * Author: Mosaic Strategies Group
  * Author URI: https://www.mosaicstg.com
  */
-require_once 'includes/program.php';
+require_once 'includes/programs.php';
 require_once 'includes/organization.php';
 require_once 'includes/shortcodes.php';
