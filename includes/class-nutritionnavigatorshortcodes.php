843e7d5d47b1f734f139fb6272942d5d4d3708ca
Cesar Hernandez <cesar.hernandez1094@gmail.com>
2023-05-19 13:02:45 -0400

    fix: testing PHPCBF formatting

--- includes/class-nutritionnavigatorshortcodes.php
+++ includes/class-nutritionnavigatorshortcodes.php
@@ -18,9 +18,9 @@ class NutritionNavigatorShortcodes {
 
 	public function __construct() {
 		// WordPress action hooks.
-		add_action('init', [$this, 'init']);
-		add_action('wp_enqueue_scripts', [$this, 'wp_enqueue_scripts']);
-		add_action('wp_footer', [$this, 'wp_footer']);
+		add_action( 'init', array( $this, 'init' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+		add_action( 'wp_footer', array( $this, 'wp_footer' ) );
 	}
 
 	public function init() {
@@ -30,8 +30,8 @@ class NutritionNavigatorShortcodes {
 	public function wp_enqueue_scripts() {
 		wp_register_script(
 			'nutrition-navigator',
-			plugin_dir_url(__DIR__) . 'dist/nutrition-navigator/index.js',
-			[],
+			plugin_dir_url( __DIR__ ) . 'dist/nutrition-navigator/index.js',
+			array(),
 			null,
 			true
 		);
@@ -39,31 +39,31 @@ class NutritionNavigatorShortcodes {
 		// Load React app build styles.
 		wp_register_style(
 			'nutrition-navigator',
-			plugin_dir_url(__DIR__) . 'dist/nutrition-navigator/index.css',
-			[],
+			plugin_dir_url( __DIR__ ) . 'dist/nutrition-navigator/index.css',
+			array(),
 			null
 		);
 	}
 
 	public function add_shortcodes() {
-		add_shortcode('nutrition_navigator', [$this, 'nutrition_navigator_shortcode']);
+		add_shortcode( 'nutrition_navigator', array( $this, 'nutrition_navigator_shortcode' ) );
 	}
 
 	public function nutrition_navigator_shortcode() {
 		$this->shortcode_rendered = true;
 
 		// Only loads styles when shortcode is rendered.
-		wp_enqueue_style('nutrition-navigator');
+		wp_enqueue_style( 'nutrition-navigator' );
 
 		return '<div id="nutrition-navigator"></div>';
 	}
 
 	public function wp_footer() {
-		if (!$this->shortcode_rendered) {
+		if ( ! $this->shortcode_rendered ) {
 			return;
 		}
 
-		wp_print_scripts('nutrition-navigator');
+		wp_print_scripts( 'nutrition-navigator' );
 	}
 }
 
