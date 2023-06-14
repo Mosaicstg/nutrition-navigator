e82818acaff1990650da0e3d11ca7a2c762221ba
Cesar Hernandez <cesar.hernandez1094@gmail.com>
2023-06-14 17:58:25 -0400

    fix(react): improve types for custom hook

--- src/hooks/useProgramTypes.tsx
+++ src/hooks/useProgramTypes.tsx
@@ -1,6 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchApi } from '../api/fetch.ts';
 
+type ProgramType = {
+  id: number;
+  count: number;
+  name: string;
+  slug: string;
+  meta: {
+    icon: string;
+  };
+  link: string;
+};
+
 const fetchAllProgramTypes = () => {
   return fetchApi('/wp-json/wp/v2/program-type?per_page=100').then((res) =>
     res.json()
@@ -8,7 +19,7 @@ const fetchAllProgramTypes = () => {
 };
 
 const useProgramTypes = () => {
-  return useQuery({
+  return useQuery<ProgramType[]>({
     queryKey: ['allProgramTypes'],
     queryFn: fetchAllProgramTypes,
     // Only run query on page load or component mount
