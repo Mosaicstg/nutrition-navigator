64662452ec4b6daac6b83953cf84972b49416e89
Cesar Hernandez <cesar.hernandez1094@gmail.com>
2023-06-20 14:20:19 -0400

    refactor(react): validate Program Types hook data using zod

--- src/hooks/useProgramTypes.tsx
+++ src/hooks/useProgramTypes.tsx
@@ -1,16 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchApi } from '../api/fetch.ts';
+import { z } from 'zod';
 
-type ProgramType = {
-  id: number;
-  count: number;
-  name: string;
-  slug: string;
-  meta: {
-    icon: string;
-  };
-  link: string;
-};
+// eslint-disable-next-line react-refresh/only-export-components
+const ProgramTypeSchema = z.object({
+  id: z.number(),
+  count: z.number(),
+  name: z.string(),
+  slug: z.string(),
+  meta: z.object({
+    icon: z.string()
+  }),
+  link: z.string()
+});
+
+type ProgramType = z.infer<typeof ProgramTypeSchema>;
 
 const fetchAllProgramTypes = () => {
   return fetchApi('/wp-json/wp/v2/program-type?per_page=100').then((res) =>
@@ -24,7 +28,19 @@ const useProgramTypes = () => {
     queryFn: fetchAllProgramTypes,
     // Only run query on page load or component mount
     retry: false,
-    select: (data) => data
+    select: (data) =>
+      data.filter((programType) => {
+        const validation = ProgramTypeSchema.safeParse(programType);
+
+        if (!validation.success) {
+          console.error(
+            `Program Type with name ${programType.name} is invalid`,
+            validation.error
+          );
+        }
+
+        return validation.success;
+      })
   });
 };
 
