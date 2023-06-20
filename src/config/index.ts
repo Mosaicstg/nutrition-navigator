a9b83e1f2d259b6158cce6bc7bc5941c21d12907
Cesar Hernandez <cesar.hernandez1094@gmail.com>
2023-06-20 17:17:33 -0400

    fix(react): resolve using right mode for testing build file in development mode

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,8 @@ import { Config as ProdConfig } from './prod.ts';
 
 const config =
   // Use local config when in development mode
-  import.meta.env.DEV ? LocalConfig : ProdConfig;
+  import.meta.env.MODE === 'development' ? LocalConfig : ProdConfig;
+
+console.log(import.meta.env);
 
 export default config;
