662883e5f108ef36d29479b8acbd849ef3086105
Cesar Hernandez <cesar@mosaicstg.com>
2025-02-24 10:23:51 -0500

    fix: add browser router

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider, createBrowserRouter } from 'react-router';
 import App from './App.tsx';
 import './index.scss';
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '*',
+    Component: App,
+    loader: ({ request }) => {
+      const searchParams = new URLSearchParams(request.url);
+      console.log(searchParams);
+    }
+  }
+]);
 
 ReactDOM.createRoot(
   document.getElementById('nutrition-navigator') as HTMLElement
 ).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
       <ReactQueryDevtools initialIsOpen={false} />
-      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
