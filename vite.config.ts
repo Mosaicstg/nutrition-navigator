770d8d2cb11566977eda4d912d063212c4e0cd19
Cesar Hernandez <cesar@mosaicstg.com>
2025-02-24 10:33:13 -0500

    feat: install vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   build: {
     target: 'es2015',
     // This ensures that assets import no larger than 5KB are turned into "data:image" urls
