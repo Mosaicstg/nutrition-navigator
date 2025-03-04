7f5924b2f8ca11ff59f730ffd09c6552fa14e7c3
Cesar Hernandez <cesar@mosaicstg.com>
2025-03-04 15:06:10 -0500

    fix: force CI script to use production env

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -1,5 +1,5 @@
 export const Config = {
-  fetchBase: '',
+  fetchBase: 'https://map.thefoodtrust.org',
   mapBoxToken:
     'pk.eyJ1IjoibW9zYWljc2VydmljZXMiLCJhIjoiY2w4ZGljbmExMDVlaDNucm9tYjk4bDFkYiJ9.OcOvWssoE-aVVSQhVKu8vQ'
 };
