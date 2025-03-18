f267ddd794980eccba92daaa82fc3a869cb36d3a
Cesar Hernandez <cesar@mosaicstg.com>
2025-03-18 15:21:41 -0400

    fix: remove unused types

--- src/routes/types.ts
+++
@@ -1,4 +0,0 @@
-import { type LoaderFunction } from 'react-router';
-
-export type LoaderData<TLoaderFn extends LoaderFunction> =
-  Awaited<ReturnType<TLoaderFn>> extends Response | infer D ? D : never;
